29fc4c7b7f0fb976ff2825908fba16feb3002893
Junaid Atique <junaidatique@gmail.com>
2018-09-13 23:49:12 +0500

    adding monogid

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,9 @@ module Postingly
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.orm :mongoid
+    end
   end
 end
