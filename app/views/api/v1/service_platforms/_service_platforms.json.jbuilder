d8ebe0a39709f8bdfc335dd7cb8098003ccb7867
Junaid Atique <junaidatique@gmail.com>
2018-11-17 12:24:02 +0500

    finalizing

--- app/views/api/v1/service_platforms/_service_platforms.json.jbuilder
+++ app/views/api/v1/service_platforms/_service_platforms.json.jbuilder
@@ -1,6 +1,7 @@
 json.service_platforms ServicePlatform.all do |service_platform|
   json.id service_platform.id.to_s
   json.name service_platform.name
+  json.share_options service_platform.share_options
   json.profiles store.profiles.where(service_platform_id: service_platform.id).connected do |profile|    
     json.servie_name profile.service.name
     json.servie_character_limit profile.service.character_limit
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -76,4 +76,15 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
+
+  # Send grid email configuration
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'yourdomain.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
